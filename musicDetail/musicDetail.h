8e8f8b6d0b8c0ba1fe2b50f5c429d5abba53689f
Breeze Shane <72027962+BreezeShane@users.noreply.github.com>
2023-07-05 15:35:35 +0800

    Finished UI.

--- musicDetail/musicDetail.h
+++ musicDetail/musicDetail.h
@@ -6,23 +6,36 @@
 #define PINEAPPLEMUSIC_MUSICDETAIL_H
 
 #include <QWidget>
+#include <QFile>
+#include "playBar/PlayBar.h"
+#include "model/CloudMusic.h"
 
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class musicDetail; }
+namespace Ui {
+    class musicDetail;
+//    class DetailedPagePlayBar;
+}
 QT_END_NAMESPACE
+
+//class DetailedPagePlayBar : public ::PlayBar {
+//public:
+//    explicit DetailedPagePlayBar(QWidget *parent = nullptr);
+//};
 
 class musicDetail : public QWidget {
 Q_OBJECT
 
 public:
     explicit musicDetail(QWidget *parent = nullptr);
+    explicit musicDetail(CloudMusic currMusic,QWidget *parent = nullptr);
     void paintEvent(QPaintEvent *event) override; // Set background pic.
 
     ~musicDetail() override;
 
 private:
     Ui::musicDetail *ui;
+    ::PlayBar *uiBar;
 };
 
 
