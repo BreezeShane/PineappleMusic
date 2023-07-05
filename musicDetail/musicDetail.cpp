71ba5d711fde5ecb576c41fa6dea37ae73dfcc7a
Breeze Shane <72027962+BreezeShane@users.noreply.github.com>
2023-07-05 10:28:43 +0800

    Finished setting background picture of musicDetail window.

--- musicDetail/musicDetail.cpp
+++ musicDetail/musicDetail.cpp
@@ -1,5 +1,25 @@
 //
-// Created by 86188 on 2023/7/4.
+// Created by breezeshane on 7/5/23.
 //
 
+// You may need to build the project (run Qt uic code generator) to get "ui_musicDetail.h" resolved
+
 #include "musicDetail.h"
+#include "ui_musicDetail.h"
+#include <QPainter>
+
+#include <qdebug.h>
+
+musicDetail::musicDetail(QWidget *parent) :
+        QWidget(parent), ui(new Ui::musicDetail) {
+    ui->setupUi(this);
+}
+
+void musicDetail::paintEvent(QPaintEvent *event) {
+    QPainter painter(this);
+    painter.drawPixmap(0,0,1024,512, QPixmap("../resource/bgp/bgp_musicDetail.jpg"));
+}
+
+musicDetail::~musicDetail() {
+    delete ui;
+}
--- musicDetail/musicDetail.h
+++ musicDetail/musicDetail.h
@@ -1,13 +1,28 @@
 //
-// Created by 86188 on 2023/7/4.
+// Created by breezeshane on 7/5/23.
 //
 
 #ifndef PINEAPPLEMUSIC_MUSICDETAIL_H
 #define PINEAPPLEMUSIC_MUSICDETAIL_H
 
+#include <QWidget>
 
-class musicDetail {
 
+QT_BEGIN_NAMESPACE
+namespace Ui { class musicDetail; }
+QT_END_NAMESPACE
+
+class musicDetail : public QWidget {
+Q_OBJECT
+
+public:
+    explicit musicDetail(QWidget *parent = nullptr);
+    void paintEvent(QPaintEvent *event) override; // Set background pic.
+
+    ~musicDetail() override;
+
+private:
+    Ui::musicDetail *ui;
 };
 
 
